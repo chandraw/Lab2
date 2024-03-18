f384a09d02f7b1acd7e8b6d6e340b8123534978a
chandraw9 <chandraw9@gmail.com>
2024-03-18 12:05:33 +0800

    asd

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+# Chandra
+# Calculator to add two numbers and display the result 
+# 1. Input
+num1 = input("Input your first number: ")
+num2 = input("Input your second number: ")
+
+# 2. Process
+sum = int(num1) + int(num2)
+
+# 3. Output
+print(f"The sum of {num1} and {num2} is",sum)
